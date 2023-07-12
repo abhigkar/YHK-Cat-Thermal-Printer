d8f73e78c0b88e81c8dd0a4dc3573c8d70dd722b
abhinav.golwalkar@gmail.com <abhinav.golwalkar@gmail.com>
2023-07-12 19:24:56 +0530

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # YHK-Cat-Thermal-Printer
-YHK type mini cat/rabbit thermal printer
+
+Mini **cat/rabbit** **thermal** printer of the **YHK** type
+
+This is yet another project with a **Cat/Rabbit thermal printer**. Other GitHub sources are also accessible, however none of them were successful for me because they all used the Cat-Printer with BLE protocol.
+
+Unfortunately, my cat-printer uses a different firmware version that is based on the Classic bluetooth protocol rather than the GATT based protocol. **YHK-XXXX** was broadcast by my printer. The last four characters of the printer's **MAC address** are XXXX.
+
+The Android and iOS app named **WalkPrint** is compatible with my cat printer. Although the app is worthless, some features need logging in.
+
+My starting point: I was motivated from [This blogpost](https://werwolv.net/blog/cat_printerhttps:/) and planned to have my own printer. I did spent some time working on the [bitbank2/Thermal_Printer](https://github.com/bitbank2/Thermal_Printer) project, but I soon found that since my printer is different so no other code will run on it.
+
+Other reference projects [repositories](https://github.com/JJJollyjim/catprinter)
+
+* [bitbank2/Thermal_Printer](https://github.com/bitbank2/Thermal_Printer)
+* [WerWolv/PythonCatPrinter](https://github.com/WerWolv/PythonCatPrinter)
+* [amber-sixel/PythonCatPrinter](https://github.com/amber-sixel/PythonCatPrinter)
+* [the6p4c/catteprinter](https://github.com/the6p4c/catteprinter)
+* [JJJollyjim/PyCatte](https://github.com/JJJollyjim/PyCatte)
+* [xssfox](https://gist.github.com/xssfox/b911e0781a763d258d21262c5fdd2dec)
+
+### Some real work of RE 🚀️
+
+In order to obtain certain internals, I have started my own reverse engineering.
+
+To examine the packet exchange between the phone and the printer, I decompiled the Android app, grabbed the BT snoop log from my phone, and then opened the log file in WireShark. And indeed, the BLE/GATT-based system was not the cause. Decompiled code for Android supports that.
+
+### Action Replay 😄
+
+Therefore, everything is straightforward. I attempted to send the same commands and data packets from my dependable Raspberry Pi Zero W through RFCOMM on the terminal based on the WirteShark logs. I finally succeeded in printing the identical image that was on my phone after a few failed attempts. In action replaySo things are simple. Based on the WirteShark logs, I tried to send the same commands/data paylods from my trusty **Raspberry pi Zero W** via **RFCOMM** on terminal. After few trial, I was able to print the same image as it was from my phone.
+
+### The Final Result 👀️
+
+To make this functional, the next task was to produce the data payload from my script. I went back and pulled three routines from the decompiled code to capture the BITMAP, transform it to 1 Bit pictures, and append some bytes as file headers. This step was more difficult because I was only able to obtain the function name. I then tried writing the similler routines in some other Python code, and it succeeded.
+
+
+### How to use the script? 🎉️
+
+1. Scan the MAC address of your printer using Bluetoothctl
+2. Run scan on if printer found run pair xx:xx:xx:xx:xx:xx ADDR and trust xx:xx:xx:xx:xx:xx
+3. Exit bluetoothctl
+4. Run sdptool add --channel=N SP, where **"N"** is the channel, remember this as you will need this in the script. I have selected 2 in my case.
+5. Run sudo rfcomm bind **N** xx:xx:xx:xx:xx:xx, N  = channel = port
+6. Run cat-printer.py
---
+++ cat-printer.py
@@ -0,0 +1,142 @@
+import socket
+import PIL.Image
+import PIL.ImageDraw
+import PIL.ImageFont
+import PIL.ImageChops
+import PIL.ImageOps
+from time import sleep
+import struct
+
+
+printerMACAddress = 'XX:XX:XX:XX:XX:XX'
+printerWidth = 384
+port = 2
+
+
+def initilizePrinter(soc):
+    soc.send(b"\x1b\x40")
+    
+def getPrinterStatus(soc):
+    soc.send(b"\x1e\x47\x03")
+    return soc.recv(38) 
+    
+def getPrinterSerialNumber(soc):
+    soc.send(b"\x1D\x67\x39")
+    return soc.recv(21)
+    
+def getPrinterProductInfo(soc):
+    soc.send(b"\x1d\x67\x69")
+    return soc.recv(16)
+    
+def sendStartPrintSequence(soc):
+    soc.send(b"\x1d\x49\xf0\x19")   
+  
+def sendEndPrintSequence(soc):
+    soc.send(b"\x0a\x0a\x0a\x0a")
+    
+    
+def trimImage(im):
+    bg = PIL.Image.new(im.mode, im.size, (255,255,255))
+    diff = PIL.ImageChops.difference(im, bg)
+    diff = PIL.ImageChops.add(diff, diff, 2.0)
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop((bbox[0],bbox[1],bbox[2],bbox[3]+10)) # don't cut off the end of the image
+
+def create_text(text, font_name="Lucon.ttf", font_size=12):
+    img = PIL.Image.new('RGB', (printerWidth, 5000), color = (255, 255, 255))
+    font = PIL.ImageFont.truetype(font_name, font_size)
+    
+    d = PIL.ImageDraw.Draw(img)
+    lines = []
+    for line in text.splitlines():
+        lines.append(get_wrapped_text(line, font, printerWidth))
+    lines = "\n".join(lines)
+    d.text((0,0), lines, fill=(0,0,0), font=font)
+    return trimImage(img)
+
+def get_wrapped_text(text: str, font: PIL.ImageFont.ImageFont,
+                     line_length: int):
+    lines = ['']
+    for word in text.split():
+        line = f'{lines[-1]} {word}'.strip()
+        if font.getlength(line) <= line_length:
+            lines[-1] = line
+        else:
+            lines.append(word)
+    return '\n'.join(lines)
+
+
+def printImage(soc, im):
+    if im.width > printerWidth:
+        # image is wider than printer resolution; scale it down proportionately
+        height = int(im.height * (printerWidth / im.width))
+        im = im.resize((printerWidth, height))
+        
+    if im.width < printerWidth:
+        # image is narrower than printer resolution; pad it out with white pixels
+        padded_image = PIL.Image.new("1", (printerWidth, im.height), 1)
+        padded_image.paste(im)
+        im = padded_image
+        
+    
+    im = im.rotate(180) #print it so it looks right when spewing out of the mouth
+    
+    # if image is not 1-bit, convert it
+    if im.mode != '1':
+        im = im.convert('1')
+        
+        
+    # if image width is not a multiple of 8 pixels, fix that
+    if im.size[0] % 8:
+        im2 = Image.new('1', (im.size[0] + 8 - im.size[0] % 8, 
+                        im.size[1]), 'white')
+        im2.paste(im, (0, 0))
+        im = im2
+        
+        
+        
+    # Invert image, via greyscale for compatibility
+    #  (no, I don't know why I need to do this)
+    im = PIL.ImageOps.invert(im.convert('L'))
+    # ... and now convert back to single bit
+    im = im.convert('1')
+
+    buf = b''.join((bytearray(b'\x1d\x76\x30\x00'), 
+                                          struct.pack('2B', int(im.size[0] / 8 % 256), 
+                                                      int(im.size[0] / 8 / 256)), 
+                                                      struct.pack('2B', int(im.size[1] % 256), 
+                                                                  int(im.size[1] / 256)), 
+                                                                  im.tobytes()))
+    initilizePrinter(soc)  
+    sleep(.5)    
+    sendStartPrintSequence(soc)
+    sleep(.5)
+    soc.send(buf)
+    sleep(.5)
+    sendEndPrintSequence(soc)
+    sleep(.5)
+
+
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.connect((printerMACAddress,port))
+
+print("Connecting to printer...")
+getPrinterStatus(s)
+sleep(0.5)
+getPrinterSerialNumber(s)
+sleep(0.5)
+getPrinterProductInfo(s)
+sleep(0.5)
+
+#Read Image File
+#img = PIL.Image.open("Turtle.jpg")
+
+#Create image from text
+#text = "Line 1\nLine 2\nLine 3"
+#img = create_text(text,font_size=65)
+
+
+printImage(s,img)
+s.close()
+
